65888fc7e95f4e570e96c598316cfe82c9dc60e2
Bruna Dahm <68978190+brudahm@users.noreply.github.com>
2021-02-25 11:11:28 -0300

    Update timeconversion.py

--- Problema 1 (Conversão das horas)/timeconversion.py
+++ Problema 1 (Conversão das horas)/timeconversion.py
@@ -6,7 +6,7 @@ def to_24(am_pm):
     seconds = am_pm[6:8]
 
     if hours > '12' or minutes > '59' or seconds > '59':
-        raise ValueError("Não é um horário válido")
+        raise ValueError("Nao e um horario valido")
 
     if meridian == 'PM' and hours != '12':
         am_pm = str(12 + int(hours)) + am_pm[2:]
